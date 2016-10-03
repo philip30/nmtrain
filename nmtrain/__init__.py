b6d2a92ddca549afb9a8522f2f0148008dc0707d
philip30 <philip.arthur30@gmail.com>
2016-10-03 09:54:29 +0900

    Added chainer components

--- nmtrain/__init__.py
+++ nmtrain/__init__.py
@@ -1,2 +1,6 @@
+import nmtrain.evals
+import nmtrain.chainer
+
 from nmtrain.batch import BatchManager, Batch
 from nmtrain.vocabulary import Vocabulary
+
---
+++ nmtrain/chainer/__init__.py
@@ -0,0 +1,2 @@
+from nmtrain.chainer.functions.cross_entropy import CrossEntropy, cross_entropy
+from nmtrain.chainer.links.linear_interpolation import LinearInterpolation, linear_interpolation
